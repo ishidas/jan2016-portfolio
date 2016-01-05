b1bcac4f95e1fdc2f480b47b7698fe6b6e40d1c7
Sawako Ishida <sawako.yoriko@gmail.com>
2016-01-04 20:25:22 -0800

    added portfolioinfo file which has content, and javascrip obj constructor created

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+function schools (data){
+  this.schoolName = data.schoolName;
+  this.degree = data.degree;
+  this.major = data.major;
+  this.schoolLink = data.schoolLink;
+  this.status = data.status
+}
+
+school.prototype.toHTML = function (){
+  var $newContentBox = $('')
+}
