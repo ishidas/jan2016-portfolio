9169080a9aec57a30768ca079cdd4a648dbed58e
Sawako Ishida <sawako.yoriko@gmail.com>
2016-01-04 21:58:46 -0800

    added some contents but need to fix school names

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
+var schoolObj = [];
+// var jobObj = [];
 
-
-function schools (data){
-  this.schoolName = data.schoolName;
-  this.degree = data.degree;
-  this.major = data.major;
-  this.schoolLink = data.schoolLink;
-  this.status = data.status
+function Schools (sth){
+  this.schoolName = sth.schoolName;
+  this.degree = sth.degree;
+  this.major = sth.major;
+  this.schoolLink = sth.schoolLink;
+  this.status = sth.status
 }
 
-school.prototype.toHTML = function (){
-  var $newContentBox = $('')
-}
+Schools.prototype.toHTML = function (){
+  var $newContentBox = $('article.edutemplate').clone();
+  var name = $newContentBox.data('schoolName',this.schoolName);
+  $('#eduname').append(this.schoolName);
+  $newContentBox.append('<p>'+this.degree+'</p>'+ '<p>'+this.major+'</p>'+'<p>'+ this.status +'</p>');
+
+  $newContentBox.append('hr');
+  $newContentBox.removeClass('edutemplate');
+  return $newContentBox;
+};
+
+school.forEach(function(obj){
+  schoolObj.push(new Schools(obj));
+})
+
+schoolObj.forEach(function(a){
+  $('#edu').append(a.toHTML())
+});
