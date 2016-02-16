0c16ff91b54e0fb95fcbd45a61321b19dc81fc67
Sawako Ishida <sawako.yoriko@gmail.com>
2016-02-16 14:31:46 -0800

    added repo projects as a list of links. Also added figcaptions to Home photos.

--- scripts/galleryView.js
+++ scripts/galleryView.js
@@ -2,16 +2,18 @@
   var galleryView = {};
 
   var render = function(repo){
-    var h4Tag = document.createElement('h4');
+    var h4Tag = document.createElement('a');
+    var liEl = document.createElement('li');
     var section = $('#gallerytab');
-    $(h4Tag).text(repo.name);
-    console.log(h4Tag);
-    return h4Tag;
+    $(h4Tag).attr('href', repo.clone_url).text(repo.name + ' Created: ' + repo.created_at);
+    $(liEl).append(h4Tag);
+    console.log(liEl);
+    return liEl;
   };
 
   galleryView.index = function (){
-    $('#gallerytab').append(
-      repoData.with('name').map(render)
+    $('#gallerytab ul').append(
+      repoData.with('clone_url','name','created_at').map(render)
     );
   };
 
